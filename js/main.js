0ef1ecd2d4ddb2146628980617a3378ed0bee98e
alekseykorotenko <alekor86@gmail.com>
2023-10-30 20:03:38 +0200

    add switch and do while

--- js/main.js
+++ js/main.js
@@ -8,18 +8,44 @@ let operation = confirm('Add?')
   ? '/'
   : false;
 
-let firstNum = +prompt('Enter first number');
-let secondNum = +prompt('Enter second number');
+let firstNum;
 
-let res =
-  operation == '+'
-    ? firstNum + secondNum
-    : operation == '-'
-    ? firstNum - secondNum
-    : operation == '*'
-    ? firstNum * secondNum
-    : operation == '/'
-    ? firstNum / secondNum
-    : 'wrong data';
+do {
+  firstNum = +prompt('Enter first number');
+} while (isNaN(firstNum));
+let secondNum;
+do {
+  secondNum = +prompt('Enter second number');
+} while (isNaN(secondNum));
+
+let res;
+
+switch (operation) {
+  case '+':
+    res = firstNum + secondNum;
+    break;
+  case '-':
+    res = firstNum - secondNum;
+    break;
+  case '*':
+    res = firstNum * secondNum;
+    break;
+  case '/':
+    res = firstNum / secondNum;
+    break;
+  default:
+    'wrong data';
+}
+
+// let res =
+//   operation == '+'
+//     ? firstNum + secondNum
+//     : operation == '-'
+//     ? firstNum - secondNum
+//     : operation == '*'
+//     ? firstNum * secondNum
+//     : operation == '/'
+//     ? firstNum / secondNum
+//     : 'wrong data';
 
 document.write(`${firstNum} ${operation} ${secondNum} = ${res}`);
